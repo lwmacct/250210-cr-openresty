2353fd0a8144e13daa2c711b0f2c2da8a126a3bd
lwmacctw <lwmacct@icloud.com>
2026-05-30 13:18:20 +0800

    chore 2026-05-30 13:18:20 CST

--- cmd/docker.sh
+++ cmd/docker.sh
@@ -3,28 +3,27 @@
 __main() {
 
   {
-    # 镜像准备
-    _image1="ghcr.io/lwmacct/250210-cr-openresty:1.25.3.2-alpine-fat-t2502100"
+    _proxy="ghcr.nju.edu.cn/"
+    _proxy="1181.s.kuaicdn.cn:11818/"
+    _image1="${_proxy}ghcr.io/lwmacct/250210-cr-openresty:1.29.2.5-alpine-fat-t260530"
     _image2="$(docker images -q $_image1)"
-    if [[ "$_image2" == "" ]]; then
-      docker pull $_image1
-      _image2="$(docker images -q $_image1)"
-    fi
+    if [[ "$_image2" == "" ]]; then docker pull $_image1 && _image2="$(docker images -q $_image1)"; fi
   }
 
-  _apps_name="openresty"
-  _apps_data="/data/$_apps_name"
-  cat <<EOF | docker compose -p "$_apps_name" -f - up -d --remove-orphans
+  _app_name="250210-cr-openresty"
+  _app_data="/data/project/$_app_name"
+  cat <<EOF | docker compose -p "$_app_name" -f - up -d --remove-orphans
 services:
   main:
-    container_name: $_apps_name
+    container_name: $_app_name
     image: "$_image2"
     restart: always
     network_mode: host
     volumes:
+			- /data/deploy:/data/deploy
       - /etc/localtime:/etc/localtime:ro
       - /etc/nginx/conf.d:/etc/nginx/conf.d
-      - $_apps_data:/usr/local/openresty/nginx
+      - $_app_data:/usr/local/openresty/nginx
     environment:
       - TZ=Asia/Shanghai
 EOF
